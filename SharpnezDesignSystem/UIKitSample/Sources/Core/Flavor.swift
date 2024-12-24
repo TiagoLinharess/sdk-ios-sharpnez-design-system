2cadb0b17f0ace7b6e51894fb1537c95c8032a08
Tiago Linhares <tiagolinharessouza@gmail.com>
2024-12-24 09:32:20 -0300

    Creates sample

---
+++ SharpnezDesignSystem/UIKitSample/Sources/Core/Flavor.swift
@@ -0,0 +1,26 @@
+//
+//  Flavor.swift
+//  SharpnezDesignSystem
+//
+//  Created by Tiago Linhares on 13/11/24.
+//
+
+import SharpnezDesignSystem
+
+// MARK: Flavor Colors
+final class FlavorColors: FlavorColorsProtocol {
+    var primary: String = "#161c48"
+    var onPrimary: String = "#FFFFFF"
+    var secondary: String = "#17457f"
+    var onSecondary: String = "#FFFFFF"
+    var surface: String = "#00a1d7"
+    var onSurface: String = "#FFFFFF"
+    var background: String = "#FFFFFF"
+    var onBackground: String = "#000000"
+    var success: String = "#01970b"
+    var onSuccess: String = "#FFFFFF"
+    var warning: String = "#dbd942"
+    var onWarning: String = "#000000"
+    var error: String = "#cc0000"
+    var onError: String = "#FFFFFF"
+}
---
+++ SharpnezDesignSystem/UIKitSample/Sources/Models/SampleItem.swift
@@ -0,0 +1,29 @@
+//
+//  SampleItem.swift
+//  SharpnezDesignSystem
+//
+//  Created by Tiago Linhares on 24/12/24.
+//
+
+import UIKit
+
+struct SampleItem: Identifiable {
+    let id: UUID = .init()
+    let controller: UIViewController
+    let name: String
+    
+    static var samples: [SampleItem] = [
+        .init(controller: UIViewController(), name: "Custom Fonts"),
+        .init(controller: UIViewController(), name: "Flavor Colors"),
+        .init(controller: UIViewController(), name: "View Status Sample"),
+        .init(controller: UIViewController(), name: "UISHButton"),
+        .init(controller: UIViewController(), name: "UISHContainer"),
+        .init(controller: UIViewController(), name: "UISHFeedbackView"),
+        .init(controller: UIViewController(), name: "UISHLabel"),
+        .init(controller: UIViewController(), name: "UISHLoading"),
+        .init(controller: UIViewController(), name: "UISHLoadingView"),
+        .init(controller: UIViewController(), name: "UISHShortcut"),
+        .init(controller: UIViewController(), name: "UISHTextField"),
+        .init(controller: UIViewController(), name: "UISHPasswordField")
+    ]
+}
