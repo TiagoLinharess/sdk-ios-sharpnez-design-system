e674acaaba7ffd4e89e279a2142941b8a8f6d73d
Tiago Linhares <tiagolinharessouza@gmail.com>
2025-02-28 15:24:13 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -25,7 +25,13 @@ let package = Package(
             path: "SharpnezDesignSystem/SharpnezDesignSystem",
             sources: ["Source"],
             resources: [
-                .process("Resources")
+                .process("Resources"),
+                .copy("Resources/Fonts/Montserrat-Bold.ttf"),
+                .copy("Resources/Fonts/Montserrat-Medium.ttf"),
+                .copy("Resources/Fonts/Montserrat-Regular.ttf"),
+                .copy("Resources/Fonts/Poppins-Bold.ttf"),
+                .copy("Resources/Fonts/Poppins-Medium.ttf"),
+                .copy("Resources/Fonts/Poppins-Regular.ttf")
             ],
             swiftSettings: [
                 .define("ENABLE_LIBRARY_EVOLUTION", .when(configuration: .release)),
